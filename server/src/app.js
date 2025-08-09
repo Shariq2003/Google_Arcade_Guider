e93ee904e4b116742a8a074cc33c8fd01af03b9a
Shariq2003 <shariqsd2003@gmail.com>
2025-08-10 05:07:43 +0530

    Build Issues Fixed

--- server/src/app.js
+++ server/src/app.js
@@ -3,32 +3,7 @@ const cors = require('cors');
 const coursesRouter = require('./routes/courses');
 
 const app = express();
-
-let corsOptions;
-if (process.env.CORS) {
-    try {
-        const origins = JSON.parse(process.env.CORS);
-        corsOptions = {
-            origin: origins,
-            credentials: true,
-            optionsSuccessStatus: 200
-        };
-    } catch (error) {
-        corsOptions = {
-            origin: process.env.CORS,
-            credentials: true,
-            optionsSuccessStatus: 200
-        };
-    }
-} else {
-    corsOptions = {
-        origin: ['http://localhost:4200', 'https://google-arcade-guider.vercel.app'],
-        credentials: true,
-        optionsSuccessStatus: 200
-    };
-}
-
-app.use(cors(corsOptions));
+app.use(cors("*"));
 
 app.use(express.json());
 app.use('/api', coursesRouter);
