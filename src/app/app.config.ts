ddef9c5990014cf20f95027e354089208449b3e7
Shariq2003 <shariqsd2003@gmail.com>
2025-08-10 02:55:44 +0530

    Initialise the frontend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    
-  ]
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
+// src/app/app.ts
 import { Component, signal } from '@angular/core';
+import { UploadComponent } from './upload/upload';
+import { Loader } from './loader/loader';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [UploadComponent, Loader, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
