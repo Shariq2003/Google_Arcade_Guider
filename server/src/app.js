9f9757b24840408039bb80fed5059ba1a1e62362
Shariq2003 <shariqsd2003@gmail.com>
2025-08-10 04:09:42 +0530

    env ffixes

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const coursesRouter = require('./routes/courses');
 
 const app = express();
-app.use(cors());
+app.use(cors(process.env.CORS));
+// The above statement allows requests from the specified origins
 app.use(express.json());
 app.use('/api', coursesRouter);
 
