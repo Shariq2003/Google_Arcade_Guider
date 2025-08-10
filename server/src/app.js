b6f2ea5837e1a782b02e50e8fdecc9e509e8a9ee
Shariq2003 <shariqsd2003@gmail.com>
2025-08-10 20:54:17 +0530

    Server Build Fixing

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 const coursesRouter = require('./routes/courses');
 
 const app = express();
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
+app.use(cors({
+    origin: [
+        'https://google-arcade-guider.vercel.app',
+        'http://localhost:4200'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use('/api', coursesRouter);
