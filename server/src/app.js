9950fd611ecbf61e69e8f4745d6066d705fb9f92
Shariq2003 <shariqsd2003@gmail.com>
2025-08-10 22:27:40 +0530

    Build Issues Fixed

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,7 @@ const coursesRouter = require('./routes/courses');
 
 const app = express();
 
-app.use(cors({
-    origin:"*",
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', coursesRouter);
